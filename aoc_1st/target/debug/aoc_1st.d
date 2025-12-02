1ef40c46abc293a5ab0ce1234865cc773d57dd7e
Tomasz Kaczmarzyk <tomaszdawidkaczmarzyk@gmail.com>
2025-12-02 02:38:48 +0100

    cleaning

--- aoc_1st/target/debug/aoc_1st.d
+++
@@ -1,1 +0,0 @@
-H:\rust\aoc\aoc_1st\target\debug\aoc_1st.exe: H:\rust\aoc\aoc_1st\src\main.rs
--- aoc_1st/target/debug/deps/aoc_1st-abba9177c7f262d5.d
+++
@@ -1,5 +0,0 @@
-H:\rust\aoc\aoc_1st\target\debug\deps\aoc_1st-abba9177c7f262d5.d: src\main.rs
-
-H:\rust\aoc\aoc_1st\target\debug\deps\libaoc_1st-abba9177c7f262d5.rmeta: src\main.rs
-
-src\main.rs:
